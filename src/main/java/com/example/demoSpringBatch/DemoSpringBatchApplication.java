f6dc2ad3671c96e6a55a2d0fa923bda26864680d
nvd11@163.com <nvd11@163.com>
2018-08-11 21:29:36 +0800

    Initial commit

---
+++ src/main/java/com/example/demoSpringBatch/DemoSpringBatchApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBatchApplication.class, args);
+	}
+}
